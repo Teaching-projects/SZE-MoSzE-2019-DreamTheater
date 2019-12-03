7c6dbc72800a7a3107d266ae46ad5b9d1298c2cf
Comerick <comerick11@gmail.com>
2019-12-03 03:33:22 +0100

    ls bugfix

--- Filesystem.cpp
+++ Filesystem.cpp
@@ -100,13 +100,7 @@ void FileSystem::mkdir(string arg){
 }
 
 void FileSystem::ls(){
-    if(!currentDir->getSubFolders().empty()){
-        //call Directory class function
-        currentDir->ls();
-        return;
-    }
-    cout<<""<<endl;
-    return;
+    currentDir->ls();
 }
 
 bool FileSystem::cd(string folder){
